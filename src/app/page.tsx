63975cd687252f2147174871c33ce969f2f6044a
FFracaro <frafracaro@gmail.com>
2024-02-04 22:33:42 -0300

    feat: different uses of Image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import Image from "next/image";
+import styles from "@/app/page.module.css"
+import heroImg from "./../../public/hero.png"
+
 export default function Home() {
   return (
     <div>
-      Home
+      <Image src={heroImg} alt="" priority className={styles.img}/>
+      <Image src="https://images.pexels.com/photos/19137460/pexels-photo-19137460/free-photo-of-praia-diamante-bali.jpeg" width="300" height="200" alt=""/>
     </div>
   );
 }
